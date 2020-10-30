1d26f9109b0e3eed83aacf141364608cf679014d
Mato <72716024+we-insub@users.noreply.github.com>
2020-10-30 14:01:17 +0900

    sql lite DB를 만저보자 개념원리정도

---
+++ SQLLiteTest.py
@@ -0,0 +1,8 @@
+import sqlite3
+conn =sqlite3.connect("/home/mato/Downloads/chadwick.db")
+print("Opened database successfully")
+
+cursor = conn.execute("SELECT yearID FROM Salaries LIMIT 10")
+
+for row in cursor:
+    print("yearID=",row[0])
