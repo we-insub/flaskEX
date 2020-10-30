06e68eea4a4c71c267aac326599bc73f5e7f6d40
Mato <72716024+we-insub@users.noreply.github.com>
2020-10-30 15:02:13 +0900

    DB만들어보기 SQL

--- SQLLiteTest.py
+++ SQLLiteTest.py
@@ -1,10 +1,29 @@
-import sqlite3
-conn =sqlite3.connect("/home/mato/Downloads/chadwick.db")
-print("Opened database successfully")
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-cursor = conn.execute("SELECT yearID FROM Salaries LIMIT 10")
 
-for row in cursor:
-    print("yearID=",row[0])
-    
-    
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
+print(__name__)
+
+dbfile = os.path.join(basedir, "db.sqlite")
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+app.config['SQLALCHEMY_COMMIT_IN_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    __tablename__ = 'test_table'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+db.create_all()
+
+@app.route('/')
+def hello():
+    return 'Hello World!!'
