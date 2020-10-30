6859538e0167a2c4a3cbcc10deac39d43b51f452
Mato <72716024+we-insub@users.noreply.github.com>
2020-10-30 16:10:20 +0900

    hello.html
    html만들기

---
+++ Model,control.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask import render_template
+from models import db
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    print('Hello')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    print('basedir:{}'.format(basedir))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+    print('file:{}'. format(dbfile))
+
+    app.config['SQLALCHEMY_DATEBASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app=app
+    db.create_all()
+    app.run(host='127.0.0.1', port = 5000, debug = True)
+
