b54e9af55107c981e18e67644c297509488bd23d
Mato <72716024+we-insub@users.noreply.github.com>
2020-10-30 15:32:57 +0900

    Update SQLLiteTest.py
    주석처리완료 끝

--- SQLLiteTest.py
+++ SQLLiteTest.py
@@ -6,8 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 print(basedir)
 print(__name__)
+#os.path.abspath 절대경로 명령어
 
 dbfile = os.path.join(basedir, "db.sqlite")
+#basedir 폴더에 os.path.join 명령어를 이용해서 db.sqlite 파일로 만들어라,
 
 app = Flask(__name__)
 
@@ -21,6 +23,8 @@ class Test(db.Model):
     __tablename__ = 'test_table'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True)
+#id sms 인티저로만들어졌으며,
+#이름은 스트링 32최대로 작성이 가능하다.
 
 db.create_all()
 
